c536b6e8bb2ea8a5524d7efe4fd30d36d3494897
Hansem Ro <hansemro@outlook.com>
2022-10-17 19:37:29 -0700

    masterkeys/prosrgb: use consistent copyright message

--- keyboards/masterkeys/prosrgb/keymaps/default/keymap.c
+++ keyboards/masterkeys/prosrgb/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /* Copyright 2022 Mateusz Radomski
- * //Copyright (c) 2022 Hansem Ro <hansemro@outlook.com>
+ * Copyright (c) 2022 Hansem Ro <hansemro@outlook.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- keyboards/masterkeys/prosrgb/mbia043.c
+++ keyboards/masterkeys/prosrgb/mbia043.c
@@ -1,5 +1,5 @@
 /* SDPX-License-Identifier: GPL-2.0-or-later
- * Copyright 2022 (c) Hansem Ro
+ * Copyright (c) 2022 Hansem Ro <hansemro@outlook.com>
  */
 
 #include "hal.h"
