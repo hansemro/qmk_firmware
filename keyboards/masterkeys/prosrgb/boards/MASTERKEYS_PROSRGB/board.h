11279cecbe2b1fb4092e5fa98c2832dd17a627e8
Hansem Ro <hansemro@outlook.com>
2022-10-17 19:26:12 -0700

    masterkeys/prosrgb: add/enable MBIA043 rgb driver

--- keyboards/masterkeys/prosrgb/boards/MASTERKEYS_PROSRGB/board.h
+++ keyboards/masterkeys/prosrgb/boards/MASTERKEYS_PROSRGB/board.h
@@ -28,6 +28,27 @@
 
 #define FLASH_SIZE (0x10000 - 0x4000) // 64kB - 16kB
 
+// MCU -> MBIA_R -> MBIA_G -> MBIA_G -> MCU
+#define MBIA043_NUM_CASCADE 3 // one MBIA IC for each color channel
+// MBIA pins
+#define MBIA043_GCLK_PIN       PAL_LINE(IOPORTC, 0)
+#define MBIA043_DCLK_PIN       PAL_LINE(IOPORTA, 14)
+#define MBIA043_SDI_PIN        PAL_LINE(IOPORTC, 2) // data sent to first MBIA from MCU
+#define MBIA043_SDO_PIN        PAL_LINE(IOPORTB, 0) // data sent to CPU from last MBIA
+#define MBIA043_LE_PIN         PAL_LINE(IOPORTA, 15)
+// MBIA 3.3V power enable
+#define MBIA043_HAS_POWER_PIN
+#define MBIA043_PWRCTRL_PIN    PAL_LINE(IOPORTC, 1)
+
+#define Q1_Ctrl PAL_LINE(IOPORTC, 8)
+#define Q2_Ctrl PAL_LINE(IOPORTC, 7)
+#define Q3_Ctrl PAL_LINE(IOPORTB, 5)
+#define Q4_Ctrl PAL_LINE(IOPORTB, 4)
+#define Q5_Ctrl PAL_LINE(IOPORTB, 3)
+#define Q6_Ctrl PAL_LINE(IOPORTB, 2)
+#define Q7_Ctrl PAL_LINE(IOPORTC, 6)
+#define Q8_Ctrl PAL_LINE(IOPORTC, 5)
+
 #if !defined(_FROM_ASM_)
 #    ifdef __cplusplus
 extern "C" {
