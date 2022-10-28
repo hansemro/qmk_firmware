4bf9ef056c42a03fe876e7aa655549586e3e7ba8
Hansem Ro <hansemro@outlook.com>
2022-10-27 18:41:32 -0700

    masterkeys/prosrgb: follow k<row><column> format

--- keyboards/masterkeys/prosrgb/prosrgb.h
+++ keyboards/masterkeys/prosrgb/prosrgb.h
@@ -24,20 +24,20 @@
 // Matrix keymap
 // clang-format off
 #define LAYOUT_tkl_ansi( \
-      K00,        K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  K09,  K0A,  K0B,  K0C,      K0D,  K0E,  K61,  \
-      K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  K18,  K19,  K1A,  K1B,  K1C,  K1E,      K2E,  K62,  K63,  \
-      K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B,  K2C,  K2D,      K3E,  K68,  K69,  \
-      K30,  K31,  K32,  K33,  K34,  K35,  K36,  K37,  K38,  K39,  K3A,  K3B,        K3D,                        \
-      K40,        K42,  K43,  K44,  K45,  K46,  K47,  K48,  K49,  K4A,  K4B,        K4D,            K4E,        \
-      K50,  K51,  K52,                    K56,                    K59,  K5A,  K5B,  K5C,      K5D,  K5E,  K6E   \
+      K0C,        K1C,  K2C,  K3C,  K0D,  K1D,  K2D,  K0E,  K1E,  K2E,  K38,  K48,  K58,      K6C,  K7C,  K3D,  \
+      K00,  K10,  K01,  K11,  K02,  K12,  K03,  K13,  K04,  K14,  K05,  K15,  K06,  K16,      K07,  K17,  K08,  \
+      K20,  K30,  K21,  K31,  K22,  K32,  K23,  K33,  K24,  K34,  K25,  K35,  K26,  K36,      K27,  K37,  K28,  \
+      K40,  K50,  K41,  K51,  K42,  K52,  K43,  K53,  K44,  K54,  K45,  K55,        K46,                        \
+      K60,        K70,  K61,  K71,  K62,  K72,  K63,  K73,  K64,  K74,  K65,        K75,            K57,        \
+      K3B,  K2B,  K4B,                    K5B,                    K6B,  K7B,  K47,  K66,      K77,  K67,  K68   \
 ) { \
     /*          Col1  Col2  Col3  Col4  Col5  Col6  Col7  Col8  Col9  Col10 Col11 Col12 Col13 Col14 Col15 */ \
-    /*Row1*/ {  K10,  K12,  K14,  K16,  K18,  K1A,  K1C,  K2E,  K63,  XXX,  XXX,  XXX,  K00,  K04,  K07 },  \
-    /*Row2*/ {  K11,  K13,  K15,  K17,  K19,  K1B,  K1E,  K62,  XXX,  XXX,  XXX,  XXX,  K01,  K05,  K08 },  \
-    /*Row3*/ {  K20,  K22,  K24,  K26,  K28,  K2A,  K2C,  K3E,  K69,  XXX,  XXX,  K51,  K02,  K06,  K09 },  \
-    /*Row4*/ {  K21,  K23,  K25,  K27,  K29,  K2B,  K2D,  K68,  K0A,  XXX,  XXX,  K50,  K03,  K61,  XXX },  \
-    /*Row5*/ {  K30,  K32,  K34,  K36,  K38,  K3A,  K3D,  K5B,  K0B,  XXX,  XXX,  K52,  XXX,  XXX,  XXX },  \
-    /*Row6*/ {  K31,  K33,  K35,  K37,  K39,  K3B,  XXX,  K4E,  K0C,  XXX,  XXX,  K56,  XXX,  XXX,  XXX },  \
-    /*Row7*/ {  K40,  K43,  K45,  K47,  K49,  K4B,  K5C,  K5E,  K6E,  XXX,  XXX,  K59,  K0D,  XXX,  XXX },  \
-    /*Row8*/ {  K42,  K44,  K46,  K48,  K4A,  K4D,  XXX,  K5D,  XXX,  XXX,  XXX,  K5A,  K0E,  XXX,  XXX }   \
+    /*Row1*/ {  K00,  K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  XXX,  XXX,  XXX,  K0C,  K0D,  K0E },  \
+    /*Row2*/ {  K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  XXX,  XXX,  XXX,  XXX,  K1C,  K1D,  K1E },  \
+    /*Row3*/ {  K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  XXX,  XXX,  K2B,  K2C,  K2D,  K2E },  \
+    /*Row4*/ {  K30,  K31,  K32,  K33,  K34,  K35,  K36,  K37,  K38,  XXX,  XXX,  K3B,  K3C,  K3D,  XXX },  \
+    /*Row5*/ {  K40,  K41,  K42,  K43,  K44,  K45,  K46,  K47,  K48,  XXX,  XXX,  K4B,  XXX,  XXX,  XXX },  \
+    /*Row6*/ {  K50,  K51,  K52,  K53,  K54,  K55,  XXX,  K57,  K58,  XXX,  XXX,  K5B,  XXX,  XXX,  XXX },  \
+    /*Row7*/ {  K60,  K61,  K62,  K63,  K64,  K65,  K66,  K67,  K68,  XXX,  XXX,  K6B,  K6C,  XXX,  XXX },  \
+    /*Row8*/ {  K70,  K71,  K72,  K73,  K74,  K75,  XXX,  K77,  XXX,  XXX,  XXX,  K7B,  K7C,  XXX,  XXX }   \
 }
