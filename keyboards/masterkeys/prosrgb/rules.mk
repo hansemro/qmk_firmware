5e32678b7c7a9090581bc72cca33382102f6bcae
Hansem Ro <hansemro@outlook.com>
2022-10-30 08:05:38 -0700

    masterkeys/prosrgb: migrate some entries to info.json

--- keyboards/masterkeys/prosrgb/rules.mk
+++ keyboards/masterkeys/prosrgb/rules.mk
@@ -7,27 +7,13 @@ MCU_SERIES = HT32F165x
 MCU_LDSCRIPT = HT32F1654_PROSRGB
 MCU_STARTUP = ht32f165x
 
-BOARD = SGK_6030_V1_3
-
-# Custom RGB matrix handling
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = custom
-
 # Keys
 CUSTOM_MATRIX = lite
-KEY_LOCK_ENABLE = no
 
 # Wear-leveling driver
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = spi_flash
 
-# Other features
-RAW_ENABLE = no
-MIDI_ENABLE = no
-VIRTSER_ENABLE = no
-COMBO_ENABLE = no
-
-LAYOUTS = tkl_ansi
 
 SRC += matrix.c
 SRC += mbia043.c
