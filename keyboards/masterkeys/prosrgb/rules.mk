ebffe53f58c9f7e0db3b4b871db2497cbf4469f4
Hansem Ro <hansemro@outlook.com>
2022-11-02 18:53:14 -0700

    masterkeys/prosrgb: set CUSTOM_MATRIX=lite from info.json

--- keyboards/masterkeys/prosrgb/rules.mk
+++ keyboards/masterkeys/prosrgb/rules.mk
@@ -7,9 +7,6 @@ MCU_SERIES = HT32F165x
 MCU_LDSCRIPT = HT32F1654_PROSRGB
 MCU_STARTUP = ht32f165x
 
-# Keys
-CUSTOM_MATRIX = lite
-
 # Wear-leveling driver
 #EEPROM_DRIVER = wear_leveling
 #WEAR_LEVELING_DRIVER = spi_flash
