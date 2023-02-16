2958c3197edc7d7628add860c1516bd6b22b4082
Hansem Ro <hansemro@outlook.com>
2023-02-15 21:12:16 -0800

    vortex/pok3r_rgb: fix bootloader jump
    POK3R_RGB bootloader checks 0x20003ffc for bootloader magic.

--- keyboards/vortex/pok3r_rgb/pok3r_rgb.c
+++ keyboards/vortex/pok3r_rgb/pok3r_rgb.c
@@ -19,7 +19,7 @@
 #include "wait.h"
 #include "quantum.h"
 
-#define FMC_SBVT1 0x40080304
+#define SRAM_BOOT_MODE 0x20003ffc
 #define RESET_AP_MAGIC 0x55aafaf0
 #define RESET_IAP_MAGIC 0x55aafaf5
 
@@ -27,7 +27,7 @@ void bootloader_jump(void) {
     wait_us(10000);                // 10 ms
     chSysDisable();                // mask all interrupts
     usbDisconnectBus(&USB_DRIVER); // disconnect usb
-    *((uint32_t *)FMC_SBVT1) = RESET_IAP_MAGIC;
+    *((uint32_t *)SRAM_BOOT_MODE) = RESET_IAP_MAGIC;
     wait_us(50000); // 50 ms
     NVIC_SystemReset();
 }
