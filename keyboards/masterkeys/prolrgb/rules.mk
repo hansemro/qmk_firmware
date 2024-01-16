76e170c9307fb7eda118de7c3cfbb1f63ba95d17
Hansem Ro <hansemro@outlook.com>
2024-01-15 22:14:21 -0800

    masterkeys/prolrgb: Enable wear leveling SPI flash driver by default

--- keyboards/masterkeys/prolrgb/rules.mk
+++ keyboards/masterkeys/prolrgb/rules.mk
@@ -8,7 +8,7 @@ MCU_LDSCRIPT = HT32F1654_PROLRGB
 MCU_STARTUP = ht32f165x
 
 # Wear-leveling driver
-#EEPROM_DRIVER = wear_leveling
-#WEAR_LEVELING_DRIVER = spi_flash
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = spi_flash
 
 SRC += mbi5042.c
