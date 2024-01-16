7b7ddc9745e6d053d0b0cdd81dcd33a177a4ee13
Hansem Ro <hansemro@outlook.com>
2024-01-16 15:47:25 -0800

    masterkeys/prosrgb: define and use SRAM_BOOT_MODE
    SRAM_BOOT_MODE points to an address arbirarily chosen by stock
    bootloader for checking whether to stay in bootloader mode. FMC->SBVT[1]
    itself has little to do with the actual boot mechanism.

--- keyboards/masterkeys/prosrgb/prosrgb.c
+++ keyboards/masterkeys/prosrgb/prosrgb.c
@@ -19,6 +19,7 @@
 #include "wait.h"
 #include "mbia043.h"
 
+#define SRAM_BOOT_MODE 0x40080304
 #define RESET_AP_MAGIC 0x55aafaf0
 #define RESET_IAP_MAGIC 0x55aafaf5
 
@@ -26,7 +27,7 @@ void bootloader_jump(void) {
     wait_us(10000);                // 10 ms
     chSysDisable();                // mask all interrupts
     usbDisconnectBus(&USB_DRIVER); // disconnect usb
-    FMC->SBVT[1] = RESET_IAP_MAGIC;
+    *((volatile uint32_t *)SRAM_BOOT_MODE) = RESET_IAP_MAGIC;
     wait_us(50000); // 50 ms
     NVIC_SystemReset();
 }
