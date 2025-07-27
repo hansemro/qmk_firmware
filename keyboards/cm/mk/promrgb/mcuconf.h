690d83b8e0b1bf2cadd558ee8788a245834eb999
Hansem Ro <hansemro@outlook.com>
2025-07-27 09:29:09 +0200

    Add Cooler Master MasterKeys Pro M RGB (ANSI+ISO) support
    Includes:
    - Wear Leveling EEPROM SPI NOR flash driver
    - Working NKRO (6KRO on startup): press LShift+RShift+n to enable
    - Supports jumping to bootloader by holding Esc (bootmagic key) while
      plugging in USB
    - ANSI or ISO layout support:
        - LAYOUT_all: Unified ANSI+ISO
        - LAYOUT_ansi: ANSI
        - LAYOUT_iso: ISO
    - Via support
    
    This does not include support for RGB lighting or indicator lights for
    lock keys.

---
+++ keyboards/cm/mk/promrgb/mcuconf.h
@@ -0,0 +1,18 @@
+// Copyright 2025 Hansem Ro <hansemro@outlook.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// CK_HSE = 8 MHz crystal
+// CK_PLL = CK_HSE * 18 = 144 MHz
+// CK_SYS = CK_PLL
+// CK_AHB = CK_SYS / 2 = 72 MHz
+// CK_USB = CK_PLL / 3 = 48 MHz
+
+#include_next "mcuconf.h"
+
+#ifdef EEPROM_DRIVER
+// SPI
+#    undef  HT32_SPI_USE_SPI1
+#    define HT32_SPI_USE_SPI1 TRUE
+#endif
