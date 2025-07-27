f86d11d3463c95dd5141446d29b6e9b6b24b2b28
Hansem Ro <hansemro@outlook.com>
2025-07-27 09:26:06 +0200

    Add Cooler Master MasterKeys Pro S RGB (ANSI+ISO) support
    Includes:
    - Wear Leveling EEPROM SPI NOR flash driver
    - Working NKRO (6KRO on startup): press LShift+RShift+n to enable
    - Supports jumping to bootloader by holding Esc (bootmagic key) while
      plugging in USB
    - ANSI or ISO layout support:
        - LAYOUT_all: Unified ANSI+ISO
        - LAYOUT_tkl_ansi: ANSI
        - LAYOUT_tkl_iso: ISO
    - Via support
    
    This does not include support for RGB lighting or indicator lights for
    lock keys.

---
+++ keyboards/cm/mk/prosrgb/halconf.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Hansem Ro <hansemro@outlook.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define HAL_USE_PAL TRUE
+#define HAL_USE_USB TRUE
+
+#ifdef EEPROM_DRIVER
+#    define HAL_USE_SPI TRUE
+#    define SPI_USE_WAIT TRUE
+#    define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
+#endif
+
+#define SERIAL_USB_BUFFERS_SIZE 256
+
+#include_next <halconf.h>
